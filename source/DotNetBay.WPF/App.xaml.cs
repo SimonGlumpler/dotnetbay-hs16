3b97ebe60dc5ba2f760a6310dcb9a34e4b94f713
Simon Glumpler <simon.glumpler@students.fhnw.ch>
2016-10-31 19:06:10 +0100

    Lab02

---
+++ source/DotNetBay.WPF/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using DotNetBay.Core;
+using DotNetBay.Core.Execution;
+using DotNetBay.Data.Entity;
+using DotNetBay.Data.Provider.FileStorage;
+using DotNetBay.Interfaces;
+
+namespace DotNetBay.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public IMainRepository MainRepository { get; private set; }
+        public IAuctionRunner AuctionRunner { get; private set; }
+
+        public App()
+        {
+            MainRepository = new FileSystemMainRepository("WPF_DotNetBay_Data.json");
+            MainRepository.SaveChanges();
+
+            PopulateWithTestData();
+
+            AuctionRunner = new AuctionRunner(MainRepository);
+            AuctionRunner.Start();
+        }
+
+        private void PopulateWithTestData()
+        {
+            var memberService = new SimpleMemberService(MainRepository);
+            var service = new AuctionService(MainRepository, memberService);
+
+            if (!service.GetAll().Any())
+            {
+                var me = memberService.GetCurrentMember();
+                service.Save(new Auction
+                {
+                    Title = "My First Auction",
+                    StartDateTimeUtc = DateTime.UtcNow.AddSeconds(10),
+                    EndDateTimeUtc = DateTime.UtcNow.AddDays(14),
+                    StartPrice = 72,
+                    Seller = me
+                });
+            }
+        }
+    }
+}
---
+++ source/DotNetBay.WPF/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+using DotNetBay.Core;
+using DotNetBay.Data.Entity;
+
+namespace DotNetBay.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private ObservableCollection<Auction> auctions;
+        private AuctionService auctionService;
+
+        public ObservableCollection<Auction> Auctions
+        {
+            get { return auctions; }
+            private set
+            {
+                this.auctions = value;
+                NotifyPropertyChanged("Auctions");
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            var app = Application.Current as App;
+            auctionService = new AuctionService(app.MainRepository, new SimpleMemberService(app.MainRepository));
+            auctions = new ObservableCollection<Auction>(auctionService.GetAll());
+            DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var sellView = new SellView();
+            sellView.ShowDialog();
+            var allAuctions = auctionService.GetAll();
+            Auctions = new ObservableCollection<Auction>(allAuctions);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
